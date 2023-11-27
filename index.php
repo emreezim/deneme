a7cef223093b7aa5c59c2bfef6958879cfe559fb
Emre Ezim <emreezim@hotmail.com>
2023-11-28 00:13:48 +0300

    asdsad

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <html>
 <body>
-<form name="uyelik" method="post">
-    <table>
-        <tr>
-        <td align="right">ad:</td>
-        <td><input type="text" name="adi"> </td>
-        </tr>
-        <tr>
-        <td align="right">Soyad:</td>
-        <td><input type="text" name="soyadi" align="right"> </td>
-        </tr>
-        <tr>
-        <td align="right">Sifre:</td>
-        <td><input type="password" name="sifre"></td>
-        </tr>
-        <tr>
-        <td align="right">Sil:</td>
-        <td><input type="Reset" name="Sil"><br></td>
-        </tr>
-        
-    </table>    
-      
-        
-
-    </form>
+<?php
+echo sadsa;
+?>
+    
    
     
 </body>
